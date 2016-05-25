5ca3cbed99ba9e69d22d50a6def9147006bcde76
Aicha Diakite <aicha.diak1994@gmail.com>
2016-05-25 17:37:41 +0200

    debut du TP

--- todo.js
+++ todo.js
@@ -5,7 +5,7 @@ var app = express();
 var EventList = require('events').EventEmitter;
 
 app.get('/', function(req, res) {
-    res.render('todo.ejs', {compteur: req.params.nombre, noms: noms});
+    res.render('todo.ejs', {});
 }).use(function(req, res, next){
     res.setHeader('Content-Type','text/plain');
     res.status(400).send('Page introuvable !');
