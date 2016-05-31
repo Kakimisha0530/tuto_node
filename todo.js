6af777253c9242d3763bd80eb1e3a623c761c126
Aicha Diakite <aicha.diak1994@gmail.com>
2016-05-31 17:22:51 +0200

    Ajout du TP "Todo List" et mise à jour du gestionnaire de packages

--- todo.js
+++ todo.js
@@ -1,12 +1,41 @@
 var express = require('express');
-var Session = require('cookie-session');
+var session = require('cookie-session');
+var bodyParser = require('body-parser');
 
 var app = express();
 var EventList = require('events').EventEmitter;
 
-app.get('/', function(req, res) {
-    res.render('todo.ejs', {});
-}).use(function(req, res, next){
+/* On utilise les sessions */
+app.use(session({secret: 'my_session_todo'}));
+// these statements config express to use these modules, and only need to be run once
+app.use(bodyParser.json());         
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/* On utilise les sessions */
+/* S'il n'y a pas de todolist dans la session,
+on en crée une vide sous forme d'array avant la suite */
+app.use(function(req, res, next){
+    if (typeof(req.session.todolist) == 'undefined') {
+        req.session.todolist = [];
+    }
+    next();
+})
+.get('/', function(req, res) {
+    res.render('todo.ejs', {todolist:req.session.todolist});
+})
+.post('/ajouter', function(req, res) {
+    if (req.body.todoval != '') {
+        req.session.todolist.push(req.body.todoval);
+    }
+    res.redirect("/");
+})
+.get('/supprimer/:index', function(req, res) {
+    if (req.params.index != '') {
+        req.session.todolist.splice(req.params.index , 1);
+    }
+    res.redirect("/");
+})
+.use(function(req, res, next){
     res.setHeader('Content-Type','text/plain');
     res.status(400).send('Page introuvable !');
 });
